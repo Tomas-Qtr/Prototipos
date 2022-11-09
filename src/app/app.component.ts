d4a3c2a1ede215b20d1a61c4e31c4aedf2b63e4f
Tomas-Qtr <tomi30.quinteros@gmail.com>
2022-11-09 03:36:12 -0300

    nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'protipos';
+
+  
+
+  constructor() {}
+
+  ngOnInit(): void {
+    const btnmenu = document.querySelector(".nav_toggle")
+    const menu = document.querySelector(".nav_menu")
+    const menu2= document.querySelector(".nav_administracion")
+    btnmenu?.addEventListener("click", function(){
+      menu?.classList.toggle("nav_menu_visible")
+      menu2?.classList.toggle("nav_menu_visible")
+    })
+
+    //medotodo scrol
+    window.addEventListener("scroll", function(){
+      var header = document.querySelector("header")
+      header?.classList.toggle("abajo", window.scrollY>50)
+    })
+  }
+
 }
