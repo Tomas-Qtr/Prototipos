f99c3a96caf4a7d3bbd30b6d091d3fd6ad68a29a
Tomas-Qtr <tomi30.quinteros@gmail.com>
2022-11-17 03:12:59 -0300

    hacemo el componente seccion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { SeccionComponent } from './seccion/seccion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    SeccionComponent
   ],
   imports: [
     BrowserModule,
